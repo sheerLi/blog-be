e1ea2866141a0ec8667dda5f8525ad33ac84cb28
sheerLi <m17805850283@163.com>
2024-02-22 23:02:59 +0800

    build: 更换http驱动

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
-import { exp3 } from './example/exp3';
 
 async function bootstrap() {
-    exp3();
-    const app = await NestFactory.create(AppModule);
+    // 使用fastify驱动
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+        // 启用跨域访问
+        cors: true,
+        // 只使用error和warn这两种输出，避免在控制台冗余输出
+        logger: ['error', 'warn'],
+    });
+    app.setGlobalPrefix('api');
     await app.listen(3000);
 }
 bootstrap();
